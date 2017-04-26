270f447fe64d4b63984e49e1670d98a79788a5ab
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-04-26 15:27:05 +0200

    Cross component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CookBook';
+
+  loadedFeature = 'recipe';
+
+  onSelectedFeature(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
