6c6c1cce7e03139762efead451e6c3ac370101ec
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-10 20:36:03 +0200

    Fixed the display of newly added ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,6 +20,11 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 
   // onNewIngredient(ingredient: Ingredient) {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
 import { Ingredient } from '../shared/ingrident.model';
+import { EventEmitter } from "@angular/core";
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -12,6 +15,7 @@ export class ShoppingListService {
 
   addNewIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
   // onNewIngredient(ingredient: Ingredient) {
