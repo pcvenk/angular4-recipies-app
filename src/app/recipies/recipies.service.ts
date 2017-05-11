d355ea1650e585e4442237526156e1e6d15143ab
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-11 08:51:15 +0200

    Updating recipies to include ingredients

--- src/app/recipies/recipies.service.ts
+++ src/app/recipies/recipies.service.ts
@@ -1,14 +1,31 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipie.model';
+import { Ingredient } from "../shared/ingrident.model";
 
 
 export class RecipeService {
   onSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super tasty Schnitzel - awesome',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Big Juicy Burger',
+      'Burger for the hungry ones',
+      'https://en.wikipedia.org/wiki/Umami_Burger#/media/File:Umami_Burger_hamburger.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2)
+      ]
+    )
   ];
 
   getRecipes() {
