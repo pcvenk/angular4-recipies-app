1b729feac0e65af014cec89892b10ae1779dba80
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-24 14:47:50 +0200

    Unsubscribing from the custom Observable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Ingredient } from "../shared/ingrident.model";
 import { ShoppingListService } from "./shopping-list.service";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
   // ingredients: Ingredient[] = [
   //   new Ingredient('Apples', 5),
@@ -15,16 +16,21 @@ export class ShoppingListComponent implements OnInit {
   // ];
 
   ingredients: Ingredient[];
+  private ingredientsSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged.subscribe(
+    this.ingredientsSubscription = this.shoppingListService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     )
+  }
+
+  ngOnDestroy() {
+    this.ingredientsSubscription.unsubscribe();
   }
 
   // onNewIngredient(ingredient: Ingredient) {
