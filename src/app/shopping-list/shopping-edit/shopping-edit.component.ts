75744c1fa8b1af93e6042f290d92783da622ace9
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-01 17:32:39 +0200

    FromSubmission via ngForm

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,11 +1,11 @@
 import {
   Component,
   OnInit,
-  ViewChild,
-  ElementRef
+  ViewChild
 } from '@angular/core';
 import { Ingredient } from "../../shared/ingrident.model";
 import { ShoppingListService } from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -15,20 +15,21 @@ import { ShoppingListService } from "../shopping-list.service";
 export class ShoppingEditComponent implements OnInit {
 
   //viewChild enables the local reference to the input fields
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  // @ViewChild('nameInput') nameInputRef: ElementRef;
+  // @ViewChild('amountInput') amountInputRef: ElementRef;
+
+  @ViewChild('form') submittedForm: NgForm;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  addIngridient() {
-
-    console.log(this.nameInputRef);
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const addedIngredient = new Ingredient(name, amount);
+  onSubmit() {
+    const value = this.submittedForm.value;
+    // const name = this.nameInputRef.nativeElement.value;
+    // const amount = this.amountInputRef.nativeElement.value;
+    const addedIngredient = new Ingredient(value.name, value.amount);
 
     this.shoppingListService.addNewIngredient(addedIngredient);
 
