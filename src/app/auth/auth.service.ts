024b160a35fe54ef8cf7a41dde2f543521cd1f57
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-15 17:16:07 +0200

    Started on emmiting err event to the signup component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import * as firebase from 'firebase';
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class AuthService {
+  error: any;
   signUp(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
-        err => console.log(err)
+        (err) => {
+          this.error = err;
+        }
       );
   }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,6 +14,7 @@ export class SignupComponent implements OnInit {
   ngOnInit() {
   }
 
+
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
