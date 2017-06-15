94681db79e69d03285eb0883754caf5081a0a0ae
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-15 15:51:39 +0200

    Implementing firebase on the initialization hook of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CookBook';
 
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBMTEqQOAdj3qdU8RggEC7HQuwG2ftkZSc",
+      authDomain: "cook-book-5a0b1.firebaseapp.com",
+    })
+  }
 
   onSelectedFeature(feature: string) {
     this.loadedFeature = feature;
