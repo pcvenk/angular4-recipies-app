4da7ac88542a16c2100835671ca1954ba9fb63d5
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-06-26 19:10:59 +0200

    Auth module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,20 @@ import {AppRoutingModule} from "./app-routing.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {AuthModule} from "./auth/auth.module";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { RecipeService } from "./recipies/recipies.service";
 import {DataStorageService} from "./shared/data-storage.service";
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import {AuthService} from "./auth/auth.service";
 import {AuthGuard} from "./auth/auth-guard.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +28,8 @@ import {AuthGuard} from "./auth/auth-guard.service";
     RecipesModule,
     AppRoutingModule,
     SharedModule,
-    ShoppingListModule
+    ShoppingListModule,
+    AuthModule
 
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
